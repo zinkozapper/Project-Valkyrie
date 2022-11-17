964a73e27141dfb58bdc0408ca92f3a31833e557
zinkozapper <52176109+zinkozapper@users.noreply.github.com>
2022-11-17 08:34:24 -0600

    Initial Commit
    Initial Commit

---
+++ Generator.py
@@ -0,0 +1,6 @@
+#This is the generator py file that will be used to generate the actualy prompts
+#The app.py file will create the UI and the rest of the app.
+
+def test(test):
+    return test
+    
---
+++ app.py
@@ -0,0 +1,6 @@
+#This is the app class where everything will come together.
+#We will most likely be using pygame for the UI components.
+#Please tell the other person when you start working on something so we don't have merge issues in GitHub.
+
+import Generator # imports other py files from the same directory
+print(Generator.test('a'))
